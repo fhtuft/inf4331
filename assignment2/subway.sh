42d5d66ffd1bec2a1648475a3dc5a78d0b9197b6
Finn-Haakon Tuft <fhtuft@ifi.uio.no>
2016-09-06 22:37:11 +0200

    subway working!

--- assignment2/subway.sh
+++ assignment2/subway.sh
@@ -4,30 +4,33 @@ if(( $# > 0))
 then
 case $1 in
 --E)
-    echo "East"
-    page=$(wget -O -  https://ruter.no/reiseplanlegger/Stoppested/%283010360%29Blindern%20%5BT-bane%5D%20%28Oslo%29/Avganger/#st:1,sp:0,bp:0 | grep "BoardingAllowed")
+    page=$(wget -q -O -  https://ruter.no/reiseplanlegger/Stoppested/%283010360%29Blindern%20%5BT-bane%5D%20%28Oslo%29/Avganger/#st:1,sp:0,bp:0 | grep "BoardingAllowed")
 
 
 page=${page##*"Retning Sognsvann"}
-echo "East (Sognsvann/Storo) Blinder"
+echo "East (Sognsvann/Storo) Blindern"
 echo "$page" | grep -oh \"departureTime\":\"[0-9][0-9]:[0-9][0-9] |grep -oh [0-9][0-9]:[0-9][0-9] 
-
-
     exit
     ;;
 --W)
-    echo "West"
-    page=$(wget -O -  https://ruter.no/reiseplanlegger/Stoppested/%283010360%29Blindern%20%5BT-bane%5D%20%28Oslo%29/Avganger/#st:1,sp:0,bp:0 | grep "BoardingAllowed" | grep -oh \"departureTime\":\"[0-9][0-9]:[0-9][0-9] |grep -oh [0-9][0-9]:[0-9][0-9] )
+    
+    page=$(wget -q -O -  https://ruter.no/reiseplanlegger/Stoppested/%283010360%29Blindern%20%5BT-bane%5D%20%28Oslo%29/Avganger/#st:1,sp:0,bp:0 | grep "BoardingAllowed")
 
-#page=${page%%*departureTime}
-echo "Departue time for next lines from Blinder"
-echo "$page"
 
+page=${page%%"Retning Sognsvann"*}
+echo "West (Majorstua) Blindern"
+echo "$page" | grep -oh \"departureTime\":\"[0-9][0-9]:[0-9][0-9] |grep -oh [0-9][0-9]:[0-9][0-9] 
+   
     exit
     ;;
     
 Nydalen)
-    echo "Nydalen"
+page=$(wget -q -O - https://ruter.no/reiseplanlegger/Stoppested/%283012130%29Nydalen%20%5BT-bane%5D%20%28Oslo%29/Avganger/#st:1,sp:0,bp:0 | grep "BoardingAllowed" | grep -oh \"departureTime\":\"[0-9][0-9]:[0-9][0-9] |grep -oh [0-9][0-9]:[0-9][0-9] )
+
+#page=${page%%*departureTime}
+echo "Departue times  from Nydalen"
+echo "$page"
+
     exit
     ;;
 
@@ -35,10 +38,10 @@ esac
 
 fi
 
-page=$(wget -O -  https://ruter.no/reiseplanlegger/Stoppested/%283010360%29Blindern%20%5BT-bane%5D%20%28Oslo%29/Avganger/#st:1,sp:0,bp:0 | grep "BoardingAllowed" | grep -oh \"departureTime\":\"[0-9][0-9]:[0-9][0-9] |grep -oh [0-9][0-9]:[0-9][0-9] )
+page=$(wget -q -O -  https://ruter.no/reiseplanlegger/Stoppested/%283010360%29Blindern%20%5BT-bane%5D%20%28Oslo%29/Avganger/#st:1,sp:0,bp:0 | grep "BoardingAllowed" | grep -oh \"departureTime\":\"[0-9][0-9]:[0-9][0-9] |grep -oh [0-9][0-9]:[0-9][0-9] )
 
 #page=${page%%*departureTime}
-echo "Departue time for next lines from Blinder"
+echo "Departure times for  from Blinder"
 echo "$page"
 
 
