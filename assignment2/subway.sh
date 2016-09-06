47f42137af18dcd8d952aa0baa7e9b584ba07c61
Finn-Haakon Tuft <fhtuft@ifi.uio.no>
2016-09-06 22:17:38 +0200

    east working

--- assignment2/subway.sh
+++ assignment2/subway.sh
@@ -5,10 +5,24 @@ then
 case $1 in
 --E)
     echo "East"
+    page=$(wget -O -  https://ruter.no/reiseplanlegger/Stoppested/%283010360%29Blindern%20%5BT-bane%5D%20%28Oslo%29/Avganger/#st:1,sp:0,bp:0 | grep "BoardingAllowed")
+
+
+page=${page##*"Retning Sognsvann"}
+echo "East (Sognsvann/Storo) Blinder"
+echo "$page" | grep -oh \"departureTime\":\"[0-9][0-9]:[0-9][0-9] |grep -oh [0-9][0-9]:[0-9][0-9] 
+
+
     exit
     ;;
 --W)
     echo "West"
+    page=$(wget -O -  https://ruter.no/reiseplanlegger/Stoppested/%283010360%29Blindern%20%5BT-bane%5D%20%28Oslo%29/Avganger/#st:1,sp:0,bp:0 | grep "BoardingAllowed" | grep -oh \"departureTime\":\"[0-9][0-9]:[0-9][0-9] |grep -oh [0-9][0-9]:[0-9][0-9] )
+
+#page=${page%%*departureTime}
+echo "Departue time for next lines from Blinder"
+echo "$page"
+
     exit
     ;;
     
