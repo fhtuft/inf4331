6303ac0e8b2395f83cf87ae60d975968ab1326c0
Finn-Haakon Tuft <fhtuft@ifi.uio.no>
2016-09-11 13:53:26 +0200

    wc working, must be tested!

--- assignment3/wc.py
+++ assignment3/wc.py
@@ -1,5 +1,4 @@
-#!/bin/env python3
-
+#!/usr/bin/env python3
 import sys
 import fileinput
 
@@ -7,6 +6,7 @@ assert sys.version_info >= (3,0)
 wordcount = 0
  
 for line in fileinput.input():
-    wordcount += len(line.split()=
+    wordcount += len(line.split())
 
-print(wordcount)     
+print(wordcount) 
+    
