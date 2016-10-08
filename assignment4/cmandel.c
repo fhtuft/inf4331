ea82841e7bdd2ec51a8ac71497b1794e126f896f
Finn-Haakon <fhtuft@student.matnat.uio.no>
2016-10-09 00:48:55 +0200

    makefile

--- assignment4/cmandel.c
+++ assignment4/cmandel.c
@@ -1,40 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <string.h>
 
 
-double **solver(const double xmin,const double xmax,const double ymin, const double ymax, const int Nx,const int Ny,const int max_escape_time) 
+void solver(const double xmin,const double xmax,const double ymin, const double ymax, const int Nx,const int Ny,const int max_escape_time,int **matrix) 
 {
 
-    /* Make the result matrix */
-    const size_t sizeof_array = sizeof(int)*Ny*Nx;
-    int *array = (int*)malloc(sizeof_array);
-    memset(array,0,sizeof_array);
-    int **matrix = (int**)malloc(sizeof(int*)*Ny);
-    for(int i =  0,int*tmp = array; i<Ny; i++) {
-        matrix[i]=tmp;
-        tmp+=Nx
-    }
+	int *array = *matrix;
 
-    for(int j = 0; j<Ny; j++) {
-        for(int i = 0; i<Nx; i++) {
-            const index = j*Nx+i;
-            int time = 0;
-            const double x = i*((xmax-xmin)/Nx);
-            const double y = j*((xmax-xmin)/Ny);
-            double a = 0.0,b =0.0;
-            while(time < max_escape_time) {
-                a = a*a-b*b + x;
-                b = 2*a*b + y;                                
-                if(a*a + b*b > 4.0) break;
+   	 for(int j = 0; j<Ny; j++) {
+        	for(int i = 0; i<Nx; i++) {
+            	const int index = j*Nx+i;
+            	int time = 0;
+            	const double x = i*((xmax-xmin)/Nx);
+            	const double y = j*((xmax-xmin)/Ny);
+            	double a = 0.0,b =0.0;
+            	while(time < max_escape_time) {
+                	a = a*a-b*b + x;
+                	b = 2*a*b + y;                                
+                	if(a*a + b*b > 4.0) break;
 
-                time++;
-            }    
+                	time++;
+            	}    
+
                 array[index] = time;
-        }    
-    }
 
-    return matrix
+        	}    
+    	}
+
 }
 
 
@@ -43,9 +36,36 @@ double **solver(const double xmin,const double xmax,const double ymin, const dou
 
 int main(int argc, char *argv[]) {
     
-     
+   
+	const double ymin = -2.0;
+	const double ymax = 2.0;
+ 	const double xmin = -2.0;
+ 	const double xmax = 2.0;
+	const int Nx = 100;
+	const int Ny = 100;
+	const int max_escape_time = 100;
+    	/* Make the result matrix */
+    	const size_t sizeof_array = sizeof(int)*Ny*Nx;
+    	int *array = (int*)malloc(sizeof_array);
+    	memset(array,0,sizeof_array);
+    	int **matrix = (int**)malloc(sizeof(int*)*Ny);
+	int *tmp = array;
+    	for(int i = 0; i < Ny; i++) {
+        	matrix[i]=tmp;
+        	tmp+=Nx;
+    	}
+  	solver(xmin,xmax,ymin,ymax,Nx,Ny,max_escape_time,matrix);
 
-    return 0;
+	/*Print out mandelbrot*/
+	for(int j = 0; j< Ny; j++) {
+		printf("\n");
+		for(int i = 0; i<Nx; i++) 
+			printf("%d ",matrix[j][i]);
+	}	
+
+ 
+
+    	return 0;
 }
 #endif
 
